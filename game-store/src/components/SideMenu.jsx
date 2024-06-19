e2b6fd31088882c5e87351326d9fa63d91b553a3
0110341 <124908461+0110341@users.noreply.github.com>
2024-06-18 21:30:44 -0400

    working sidebar

---
+++ game-store/src/components/SideMenu.jsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+import './sideMenu.css'
+import navListData from '../data/navListData';
+import NavListItem from './NavListItem';
+
+function SideMenu() {
+    const [navData, setNavData] = useState(navListData)
+  return (
+    <div className='sideMenu'>
+        <a href="#" className='logo'>
+            <i className="bi bi-dpad"></i>
+            <span className='brand'>gameShop</span>
+        </a>
+        <ul className="nav">
+            {navData.map(item => ( 
+              <NavListItem key={item._id} item={item}/>
+            ))}
+            
+          
+        </ul>
+    </div>
+  );
+}
+
+export default SideMenu
---
+++ game-store/src/data/navListData.js
@@ -0,0 +1,35 @@
+const navListData = [
+  {
+    _id: 1,
+    target: "home",
+    name: "Home",
+    icon: "bi bi-house",
+    active: true,
+  },
+
+  {
+    _id: 2,
+    target: "categories",
+    name: "Categories",
+    icon: "bi-window-stack",
+    active: false,
+  },
+
+  {
+    _id: 3,
+    target: "library",
+    name: "My Library",
+    icon: "bi-heart",
+    active: false,
+  },
+
+  {
+    _id: 4,
+    target: "bag",
+    name: "My Cart",
+    icon: "bi-cart",
+    active: false,
+  },
+];
+
+export default navListData;
