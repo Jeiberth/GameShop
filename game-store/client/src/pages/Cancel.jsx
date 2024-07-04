6ac9426d2180a006dc9a187aa84d8ae55f5093a5
jeiberth <jeiberth_camilo@hotmail.com>
2024-07-04 15:29:13 -0400

    Little buttom for going to the main was added in payment success/canceled

--- game-store/client/src/pages/Cancel.jsx
+++ game-store/client/src/pages/Cancel.jsx
@@ -2,12 +2,21 @@
 import React from 'react';
 import './cancel.css'
 
-const Cancel = () => {
+import { useNavigate } from 'react-router-dom';
+
+
+function Cancel(){
+const navigate = useNavigate()
+
+const handleMainClick = () => {
+    navigate('../'); // Navigate to the Register page
+  };
 return (
     <div id="contp">
     <img id="sf" width="24" height="24" src="https://img.icons8.com/material-sharp/24/FFFFFF/cancel--v1.png" alt="cancel--v1"/>
     <h1 id="Acs">Payment Cancelled</h1>
     <p id="reason">Your payment has been cancelled. Try again later.</p>
+    <img onClick={handleMainClick}  id="HP" width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/home-page--v1.png" alt="home-page--v1"/>
     </div>
 );
 };
--- game-store/client/src/pages/Success.jsx
+++ game-store/client/src/pages/Success.jsx
@@ -1,15 +1,23 @@
 
 import React from 'react';
 import './cancel.css'
+import { useNavigate } from 'react-router-dom';
 
-const Success = () => {
+
+function Success(){
+const navigate = useNavigate()
+
+const handleMainClick = () => {
+    navigate('../'); // Navigate to the Register page
+  };
+
 return (
     <div id="contp">
     <img id="sf" width="64" height="64" src="https://img.icons8.com/glyph-neue/64/FFFFFF/ok--v1.png" alt="ok--v1"/>
     <h1 id="Acs">Payment Successful</h1>
     <p id="reason">Thank you for your purchase! Your payment has been processed successfully.</p>
+    <img onClick={handleMainClick}  id="HP" width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/home-page--v1.png" alt="home-page--v1"/>
     </div>
-);
-};
-
+)
+}
 export default Success;
